109ed0a236860b5ba5a082f3d60ee25e94dde9b2
AnatoliyR <95148318+MrAnatoliy@users.noreply.github.com>
2024-02-06 19:09:13 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// Environment variables
+require('dotenv').config();
+
+//Requirements
+const express  = require('express');
+const mongoose = require('mongoose');
+
+//
+const app = express();
+mongoose.connect(process.env.DATABASE_URI);
+const db = mongoose.connection;
+
+db.on('error', (err) => console.log(err));
+db.once('open', () => console.log(`Connected to database: ${process.env.DATABASE_URI}/${db.name}`));
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started successfully on ${process.env.SERVER_URL}:${process.env.PORT} `)
+});
